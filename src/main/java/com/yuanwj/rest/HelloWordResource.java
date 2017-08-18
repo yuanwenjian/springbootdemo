2d111376ba3d977b651af67d24c23a02b490c5e1
bmk <yuanwj@biomarker.com.cn>
2017-08-18 15:37:35 +0800

    添加springboot 添加日志配置

--- src/main/java/com/yuanwj/rest/HelloWordResource.java
+++ src/main/java/com/yuanwj/rest/HelloWordResource.java
@@ -1,6 +1,8 @@
 package com.yuanwj.rest;
 
 import com.yuanwj.config.ConfigProperties;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,13 +19,15 @@ public class HelloWordResource {
     @Inject
     private ConfigProperties configProperties;
 
+    private static final Logger LOG= LoggerFactory.getLogger(HelloWordResource.class);
     @Inject
     private Environment environment;
 
     @RequestMapping(value = "helloWorld")
     public String helloWorld(){
-        System.out.println(configProperties.getRandomString());
-        System.out.println(environment.getProperty("server.port"));
+        LOG.warn(configProperties.getRandomString());
+        LOG.warn(configProperties.getSwagger().getTitle());
+        LOG.warn(environment.getProperty("server.port"));
         return configProperties.getRandomString();
     }
 
